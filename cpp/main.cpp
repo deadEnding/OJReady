e34145789f7206c37fa4d7b49d84261854b0ffe0
deadEnding <deadend.endif@gmail.com>
2016-04-08 14:56:46 +0800

    add printer code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+
+/**
+ * 功能: 遍历vector容器进行打印
+ * 说明: c++模板类/模板函数的声明与定义应该放在头文件里，不要分开来写类中函数的声明与定义(会导致连接错误)
+ */
+template <typename T>
+void printByIter(T vec)
+{
+    for(typename T::iterator it=vec.begin(); it!=vec.end(); ++it)
+    {
+        cout << *it << endl;
+    }
+}
+
+template <typename T>
+void printByIndex(T vec)
+{
+    for(typename T::size_type ix=0; ix!=vec.size(); ++ix)
+    {
+        cout << vec[ix] << endl;
+    }
+}
+
+
+void printTestCase()
+{
+    vector<int> intVec;
+    intVec.push_back(1);
+    intVec.push_back(2);
+    printByIter(intVec);
+
+    vector<char> charVec;
+    charVec.push_back('a');
+    charVec.push_back('b');
+    printByIndex(charVec);
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    printTestCase();
     return 0;
 }
