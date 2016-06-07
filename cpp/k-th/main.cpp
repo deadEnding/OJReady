cf97a30310b23d418e2fbd66c03a474e8984a7e8
deadEnding <deadend.endif@gmail.com>
2016-06-07 23:08:10 +0800

    modify k-th example

--- cpp/k-th/main.cpp
+++ cpp/k-th/main.cpp
@@ -5,11 +5,15 @@ using namespace std;
 
 /**
  * 快排的一次划分
+ *
+ * @param a: 数组a
+ * @param i: 数组a的起始下标
+ * @param j: 数组a的结束下标
+ *
+ * @return 划分后基准值的下标
  */
-int Partition(int a[], int left, int right) {
-    int i = left;
-    int j = right;
-    int x = a[left];
+int Partition(int a[], int i, int j) {
+    int x = a[i];
     while (i < j) {
         while (i < j && a[j] >= x) j--;
         if (i < j)
@@ -24,10 +28,13 @@ int Partition(int a[], int left, int right) {
 
 /**
  * 返回无序数组中第k小的元素
+ *
  * @param a: 数组a
  * @param i: 数组a的起始下标
  * @param j: 数组a的结束下标
  * @param k: a[i...j]的第k小的元素
+ *
+ * @return 第k小的元素
  */
 int SearchKth(int a[], int i, int j, int k) {
     int m = Partition(a, i, j);
@@ -44,11 +51,14 @@ int SearchKth(int a[], int i, int j, int k) {
 
 /**
  * 返回两个有序数组中第k小的元素
+ *
  * @param a: 数组a
  * @param m: 数组a的长度
  * @param b: 数组b
  * @param n: 数组b的长度
  * @param k: 第k小的元素
+ *
+ * @return 第k小的元素
  */
 int SearchKth(int a[], int m, int b[], int n, int k) {
 
@@ -77,13 +87,16 @@ int SearchKth(int a[], int m, int b[], int n, int k) {
 int main()
 {
     int a[10] = {4,2,5,6,1,0,9,8,3,7};
+
     for (int i = 1; i <= 10; i++)
         assert(SearchKth(a, 0, 9, i) == i-1);
 
-    int b[5] = {1,3,5,6,9};
-    int c[7] = {0,2,4,7,8,10,11};
+
+    int b[4] = {3,5,6,9};
+    int c[8] = {0,1,2,4,7,8,10,11};
+
     for (int i = 1; i <= 12; i++) {
-        assert(SearchKth(b, 5, c, 7, i) == i-1);
+        assert(SearchKth(b, 4, c, 8, i) == i-1);
     }
 
     cout << "Success" << endl;
